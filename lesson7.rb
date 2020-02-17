d6b3a3df25a660b2caa9c7a2233fe6d14ae0ca3a
村上幸太郎 <murakami.kotaro.infratop@gmail.com>
2020-02-17 15:17:37 +0900

    [modified]2-8

--- lesson7.rb
+++ lesson7.rb
@@ -1,8 +1,3 @@
-puts "数字を２つ入力してください"
- a=gets.to_i
- b=gets.to_i
- puts "a+b=#{a+b}"
-
 dice=0
 while dice !=6 do
 	dice=rand(1..6)
@@ -16,3 +11,51 @@ end
 {"apple"=>130,"strawberry"=>180,"orange"=>100}.each do |fruit,price|
 	puts "#{fruit}は#{price}円です"
 end
+
+d=0
+while d <=10 do
+	if d >5
+		break
+	end
+	puts d
+	d+=1
+end
+
+puts "計算を始めます"
+puts "2つの値を入力してください"
+a=gets.to_i
+b=gets.to_i
+puts "計算結果を出力します"
+puts "a*b=#{a*b}"
+puts "計算を終了します"
+
+puts "計算を始めます"
+puts "何回計算を繰り返しますか？"
+
+
+input=gets.to_i
+f=1
+
+while f <= input do
+	puts "#{f}回目の計算"
+
+	puts "2つの数字を入力してください"
+
+	g=gets.to_i
+
+	h=gets.to_i
+
+	puts "計算結果を出力します"
+
+	puts "g+h=#{g+h}"
+
+	puts "g-h=#{g-h}"
+
+	puts "g*h=#{g*h}"
+
+	puts "g/h=#{g/h}"
+
+	f += 1
+
+end
+puts "計算結果を終了します"
